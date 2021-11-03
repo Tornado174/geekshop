eb965a88c5c94b7700873b9c6d8dc87982ad8134
Oleg Salmin <olegsalmin74@gmail.com>
2021-11-03 23:30:32 +0500

    Lesson 3. Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from .models import Product, ProductCategory
+
 # Create your views here.
+
 
 def main(request):
     title = "главная"
@@ -12,19 +14,21 @@ def main(request):
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
+
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
     same_products = Product.objects.all()
     content = {
-        "title": title, 
-        "links_menu": links_menu, 
+        "title": title,
+        "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
     }
     if pk:
         print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
+
 
 def contact(request):
     title = "О нас"
