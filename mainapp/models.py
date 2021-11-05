4714709281d06c234cc3f0cab334ba360957e1b1
Oleg Salmin <olegsalmin74@gmail.com>
2021-11-06 03:52:41 +0500

    Lesson 4. Auth app and user model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.db.models.fields import BLANK_CHOICE_DASH
+
 
 
 class ProductCategory(models.Model):
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(verbose_name="имя продукта", max_length=128)
     image = models.ImageField(upload_to="products_images", blank=True)
     short_desc = models.CharField(verbose_name="краткое описание продукта", max_length=60, blank=True)
-    descriptions = models.TextField(verbose_name="описание продукта", blank=True)
+    description = models.TextField(verbose_name="описание продукта", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
 
