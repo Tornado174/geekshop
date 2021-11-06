892e5ba6b4093d674ac702b7dd68f21b350286b0
Oleg Salmin <olegsalmin74@gmail.com>
2021-11-07 03:32:17 +0500

    Lesson 4. Code styl fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name="имя", max_length=64, unique=True)
     description = models.TextField(verbose_name="описание", blank=True)
@@ -21,7 +20,6 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
-
 
 
 class Contact(models.Model):
