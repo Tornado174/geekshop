259bc98dca0f68db06f4660ecc299982b580151a
Oleg Salmin <olegsalmin74@gmail.com>
2021-11-21 02:01:01 +0500

    Lesson 8. Code style fix

--- mainapp/migrations/0006_product_is_active.py
+++ mainapp/migrations/0006_product_is_active.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0005_auto_20211116_2215'),
+        ("mainapp", "0005_auto_20211116_2215"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='product',
-            name='is_active',
-            field=models.BooleanField(default=True, verbose_name='продукт активен'),
+            model_name="product",
+            name="is_active",
+            field=models.BooleanField(default=True, verbose_name="продукт активен"),
         ),
     ]
