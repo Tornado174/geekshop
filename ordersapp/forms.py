d6bf9ff77ee219ebeb133a342cd2b37baf00e9f1
Oleg Salmin <olegsalmin74@gmail.com>
2021-12-11 04:00:10 +0500

    Lesson_14. Query renew

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from ordersapp.models import Order, OrderItem
+from ordersapp.models import Order, OrderItem, Product
 
 
 class OrderForm(forms.ModelForm):
@@ -19,6 +19,7 @@ class OrderItemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
+        self.fields["product"].queryset = Product.get_items().select_related()
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
 
